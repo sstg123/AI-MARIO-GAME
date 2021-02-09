93d4cd47195b02ba401c751b93cb72104cd88b52
sstg123 <71703766+sstg123@users.noreply.github.com>
2021-02-09 16:45:58 +0530

    Add files via upload

--- characters_environment.js
+++ characters_environment.js
@@ -228,8 +228,11 @@ function instializeInDraw(){
 // Character get coins
 function getCoins(coin,character){
   if( character.overlap(coin) && character.live && coin.get==false){
+    
     character.coins+=1;
     coin.get=true;
+    mario_coin.play();
+
   };
 }
     
@@ -318,6 +321,7 @@ function manualControl(character){
 /* Movements of character */
 function jumping(character){
 	if((noseY < 200 && character.live) || (touchIsDown&&character.live) ){
+    mario_jump.play();
 		character.velocity.y+=gameConfig.jump;
 	}
 }
@@ -367,6 +371,7 @@ function StepOnEnemy(obj1,obj2){
 	if(obj1_Right>=obj2_Left&&obj1_Left<=obj2_Right && obj1_Down<=obj2_Up+7 && obj1_Down>=obj2_Up-7 && obj2.live==true && obj2.touching.top){
 		obj2.live=false;
     obj1.killing=30;
+    mario_kick.play();
     obj1.kills++;
     if(obj1.velocity.y>=gameConfig.jump*0.8){
       obj1.velocity.y=gameConfig.jump*0.8;
@@ -385,6 +390,15 @@ function die(character){
     character.status="dead";
     character.changeAnimation('dead');
     character.velocity.y-=2;
+   
+if(character.liveNumber > 0)
+
+{
+
+mario_die.play();
+
+  }
+
 }
 
 // check character status and response to sprite and game status
@@ -396,6 +410,7 @@ function checkStatus(character){
   }
   if(character.live==false && character.liveNumber==0){
     gameConfig.status="gameover"
+    mario_gameover.play();
   }
 
 }
--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 function preload() {
-	mario_gameover = loadSound("gameover.wav");
+	world_start = loadSound("world_start.wav");
 	mario_jump = loadSound("jump.wav");
 	mario_coin = loadSound("coin.wav");
-	mario_kick = loadSound("kick.wav");
+    mario_gameover = loadSound("gameover.wav");
+    mario_kick = loadSound("kick.wav");
 	mario_die = loadSound("mariodie.wav");
-	world_start = loadSound("world_start.wav");
+
 	setSprites();
 	MarioAnimation();
 }
